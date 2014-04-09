2a7b4b5ad8dde65ebf6989e892ef30fc55a03139
wkeil <werner.keil@gmx.net>
2014-04-09 02:58:38 +0200

    OPEN - task 2: Migrate RI - Updated JavaDoc https://github.com/unitsofmeasurement/unitsofmeasurement-parent/issues/issues/issue/2

--- quantity/src/main/java/javax/measure/quantity/AmountOfSubstance.java
+++ quantity/src/main/java/javax/measure/quantity/AmountOfSubstance.java
@@ -14,7 +14,10 @@ import javax.measure.Quantity;
  * The metric system unit for this quantity is "mol" (mole).
  *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
- * @version 1.0
+ * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
+ * @version 1.1
+ * 
+ * @see <a href=" http://en.wikipedia.org/wiki/Amount_of_substance">Wikipedia: Amount of Substance</a>
  */
 public interface AmountOfSubstance extends Quantity<AmountOfSubstance> {
 }
