f7e154da9c890224a3124e4ea1847d8807181b45
Werner Keil <werner.keil@gmx.net>
2014-06-15 23:12:09 +0200

    1: JIRA Placeholder See: https://java.net/jira/browse/UNITSOFMEASUREMENT-19 Task-Url: https://github.com/unitsofmeasurement/unitsofmeasurement-parent/issues/issues/issue/1

--- core/src/main/java/javax/measure/function/BiFactory.java
+++ core/src/main/java/javax/measure/function/BiFactory.java
@@ -11,15 +11,15 @@ package javax.measure.function;
  * Represents a factory that accepts two arguments and creates a result.
  *
  * <p>This is a <a href="package-summary.html">functional interface</a>
- * whose functional method is {@link #create(Object, Object)}.
+ * whose functional method is {@link #create(T, U)}.
  *
  * @param <T> the type of the first argument to the factory
  * @param <U> the type of the second argument to the factory
  * @param <R> the type of the result of the factory
  *
  * @see Function
- * @deprecated this is only used by QuantityFactory, consider moving to RI or SE-specific module
  */
+// TODO this is only used by QuantityFactory, consider moving to RI or SE-specific module
 public interface BiFactory<T, U, R> {
 	R create(T t, U u);
 }
