fcb1b5a717a2fb365f291a85890cd2542429557b
otaviojava <otaviojava@java.net>
2014-09-17 18:00:39 -0300

    rename BifFactory

--- src/main/java/javax/measure/function/QuantityFactory.java
+++ src/main/java/javax/measure/function/QuantityFactory.java
@@ -16,11 +16,10 @@ package javax.measure.function;
  * @param <T> the type of the first argument to the factory
  * @param <U> the type of the second argument to the factory
  * @param <R> the type of the result of the factory
- * 
+ *
  * @author Werner Keil
  * @version 0.3, $Date: 2014-08-24 $
  */
-// TODO this is only used by QuantityFactory, consider replacing it with QF (which needed another name then)
-public interface BiFactory<T, U, R> {
+public interface QuantityFactory<T, U, R> {
 	R create(T t, U u);
 }
