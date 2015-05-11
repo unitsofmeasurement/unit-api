a45d3e2ac1aa560c041521315ab2a733c114493f
Werner Keil <werner.keil@gmx.net>
2015-05-11 18:16:32 +0200

    Update LuminousFlux.java
    Table 3, see http://physics.nist.gov/cuu/Units/units.html, so keeping it

--- src/main/java/javax/measure/quantity/LuminousFlux.java
+++ src/main/java/javax/measure/quantity/LuminousFlux.java
@@ -1,6 +1,6 @@
 /**
  * Unit-API - Units of Measurement API for Java
- * Copyright (c) 2014 Jean-Marie Dautelle, Werner Keil, V2COM
+ * Copyright (c) 2014-2015 Jean-Marie Dautelle, Werner Keil, V2COM
  * All rights reserved.
  *
  * See LICENSE.txt for details.
@@ -9,14 +9,12 @@ package javax.measure.quantity;
 
 import javax.measure.Quantity;
 
-
 /**
  * Luminous flux.
  * The metric system unit for this quantity is "lm" (lumen).
  *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
- * @version 1.0
- * @deprecated see https://java.net/jira/browse/UNITSOFMEASUREMENT-100
+ * @version 1.1
  */
 public interface LuminousFlux extends Quantity<LuminousFlux> {
 }
