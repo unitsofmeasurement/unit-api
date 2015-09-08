a905f1e7788129987bdee50d62c400a1e6ffcf54
Werner Keil <werner.keil@gmx.net>
2015-09-08 17:15:56 +0200

    Update MagneticFieldStrength.java
    Used standard naming

--- src/main/java/javax/measure/quantity/MagneticFieldStrength.java
+++ src/main/java/javax/measure/quantity/MagneticFieldStrength.java
@@ -1,6 +1,6 @@
-/**
+/*
  * Unit-API - Units of Measurement API for Java
- * Copyright (c) 2014 Jean-Marie Dautelle, Werner Keil, V2COM
+ * Copyright (c) 2014-2015 Jean-Marie Dautelle, Werner Keil, V2COM
  * All rights reserved.
  *
  * See LICENSE.txt for details.
@@ -9,14 +9,13 @@ package javax.measure.quantity;
 
 import javax.measure.Quantity;
 
-
 /**
  * Magnetic field strength.
- * The system unit for this quantity is "A/m" (ampere per meter).
+ * The system unit for this quantity is "A/m" (ampere per metre).
  *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.0.2
+ * @version 1.0.3
  */
 public interface MagneticFieldStrength extends Quantity<MagneticFieldStrength> {
 }
