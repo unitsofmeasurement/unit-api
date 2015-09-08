5117caaf39cb49af2c20bf77047288725d1e1880
Werner Keil <werner.keil@gmx.net>
2015-09-08 17:10:56 +0200

    Update ElectricPermittivity.java
    Fixed JavaDoc, using standard naming

--- src/main/java/javax/measure/quantity/ElectricPermittivity.java
+++ src/main/java/javax/measure/quantity/ElectricPermittivity.java
@@ -1,6 +1,6 @@
-/**
+/*
  * Unit-API - Units of Measurement API for Java
- * Copyright (c) 2014 Jean-Marie Dautelle, Werner Keil, V2COM
+ * Copyright (c) 2014-2015 Jean-Marie Dautelle, Werner Keil, V2COM
  * All rights reserved.
  *
  * See LICENSE.txt for details.
@@ -12,11 +12,11 @@ import javax.measure.Quantity;
 
 /**
  * How an electric field affects, and is affected, by a dielectric medium.
- * The system unit for this quantity is "F/m" (farads per meter).
+ * The system unit for this quantity is "F/m" (farads per metre).
  *
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.8
+ * @version 1.8.1
  *
  * @see <a href="http://en.wikipedia.org/wiki/Electric_permittivity">Wikipedia: Electric Permittivity</a>
  */
