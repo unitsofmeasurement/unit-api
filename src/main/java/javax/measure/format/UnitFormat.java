ec558cb79cb4d59b53894ff835a284df28b0502f
Werner Keil <werner.keil@gmx.net>
2015-09-17 15:26:10 +0200

    Update UnitFormat.java
    Updated JavaDoc

--- src/main/java/javax/measure/format/UnitFormat.java
+++ src/main/java/javax/measure/format/UnitFormat.java
@@ -26,7 +26,7 @@ import javax.measure.Unit;
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
  *
- * @version 0.7, Sep 14, 2015
+ * @version 0.7.1, Sep 17, 2015
  *
  * @see Unit
  * @see Parser
@@ -51,31 +51,32 @@ public interface UnitFormat extends Parser<CharSequence, Unit<?>> {
      */
     String format(Unit<?> unit);
     
-	/**
-	 * Attaches a system-wide label to the specified unit.<p>
-	 * If the specified label is already associated to a unit the previous
-	 * association can be discarded or ignored. 
-	 * Depending on the {@link UnitFormat} implementation, this call may be ignored if the particular unit already has a label.</p>
-	 * If a {@link UnitFormat} implementation is explicitly <b>immutable</b>, similar to e.g. the result of <tt>Collections.unmodifiableList()</tt>, then an {@linkplain UnsupportedOperationException} may be thrown upon this call.<p>
-	 * Since <tt>UnitFormat</tt> implementations are often singletons, <b>system-wide</b> means, the label applies to every instance of <tt>UnitFormatA</tt> implementing <tt>UnitFormat</tt> in this case, but not every instance of <tt>UnitFormatB</tt> or <tt>UnitFormatC</tt> both also implementing <tt>UnitFormat</tt>.  
-	 * </p>
-	 * @param unit
-	 *            the unit being labeled.
-	 * @param label
-	 *            the new label for this unit.
-	 * @throws IllegalArgumentException
-	 *             if the label is not a valid identifier.
-	 * @throws UnsupportedOperationException if the <tt>label</tt> operation
+    /**
+     * Attaches a system-wide label to the specified unit.<p>
+     * If the specified label is already associated to a unit the previous
+     * association can be discarded or ignored. 
+     * Depending on the {@link UnitFormat} implementation, this call may be ignored if the particular unit already has a label.</p>
+     * If a {@link UnitFormat} implementation is explicitly <b>immutable</b>, similar to e.g. the result of <tt>Collections.unmodifiableList()</tt>, then an {@linkplain UnsupportedOperationException} may be thrown upon this call.<p>
+     * Since <tt>UnitFormat</tt> implementations often apply the Singleton pattern, <b>system-wide</b> means, the label applies to every instance of <tt>UnitFormatA</tt> implementing <tt>UnitFormat</tt> in this case, but not every instance of <tt>UnitFormatB</tt> or <tt>UnitFormatC</tt> both also implementing <tt>UnitFormat</tt>.  
+     * If a <tt>UnitFormat</tt> #isLocaleSensitive() it is up to the implementation, whether the label is ignored, applied in a local-neutral manner (in addition to its local-sensitive information) or locale-specific.
+     * </p>
+     * @param unit
+     *            the unit being labeled.
+     * @param label
+     *            the new label for this unit.
+     * @throws IllegalArgumentException
+     *             if the label is not a valid identifier.
+     * @throws UnsupportedOperationException if the <tt>label</tt> operation
      *         is not supported by this {@link UnitFormat}
-	 */
-	void label(Unit<?> unit, String label);
+     */
+    void label(Unit<?> unit, String label);
     
-	/**
-	 * Returns <code>true</code> if this {@link UnitFormat} depends on a <code>Locale</code> to perform its tasks.<p>
-	 * In environments that do not support a <code>Locale</code>, e.g. Java ME, this usually returns <code>false</code>.</p>
-	 * @return Whether this format depends on the locale.
-	 */
-	boolean isLocaleSensitive();
+    /**
+     * Returns <code>true</code> if this {@link UnitFormat} depends on a <code>Locale</code> to perform its tasks.<p>
+     * In environments that do not support a <code>Locale</code>, e.g. Java ME, this usually returns <code>false</code>.</p>
+     * @return Whether this format depends on the locale.
+     */
+    boolean isLocaleSensitive();
 	
     /**
      * Parses a portion of the specified {@code CharSequence} from the
