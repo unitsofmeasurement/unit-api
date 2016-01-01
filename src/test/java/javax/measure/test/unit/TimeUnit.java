f2536630b68e8df1f5af4d58bff55571fe686101
Werner Keil <werner.keil@gmx.net>
2016-01-01 14:53:53 +0100

    Updated current year in headers
    Signed-off-by: Werner Keil <werner.keil@gmx.net>

--- src/test/java/javax/measure/test/unit/TimeUnit.java
+++ src/test/java/javax/measure/test/unit/TimeUnit.java
@@ -1,6 +1,6 @@
 /*
  * Units of Measurement API
- * Copyright (c) 2014-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ * Copyright (c) 2014-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
  *
  * All rights reserved.
  *
