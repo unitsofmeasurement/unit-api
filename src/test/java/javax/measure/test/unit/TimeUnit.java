0f3597814b088244fc177fc4bb8d692b6bb7aac9
Werner Keil <werner.keil@gmx.net>
2018-01-13 00:18:00 +0100

    61: Update Headers
    Task-Url: https://github.com/unitsofmeasurement/unit-api/issues/issues/61

--- src/test/java/javax/measure/test/unit/TimeUnit.java
+++ src/test/java/javax/measure/test/unit/TimeUnit.java
@@ -13,7 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
  *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
- * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products
+ * 3. Neither the name of JSR-385 nor the names of its contributors may be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
