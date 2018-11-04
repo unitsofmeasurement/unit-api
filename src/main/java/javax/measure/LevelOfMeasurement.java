15a5e207dcc678b1e6731c7d3292061eac5190a4
Werner Keil <werner.keil@gmx.net>
2018-11-05 00:47:34 +0100

    141: LevelOfMeasurement.isGreaterThanOrEqualTo duplicate standard API
    Task-Url: https://github.com/unitsofmeasurement/unit-api/issues/issues/141

--- src/main/java/javax/measure/LevelOfMeasurement.java
+++ src/main/java/javax/measure/LevelOfMeasurement.java
@@ -34,7 +34,7 @@ package javax.measure;
  *
  * @see <a href="https://en.wikipedia.org/wiki/Level_of_measurement#Stevens's_typology">Wikipedia: Level of measurement - Stevens's typology</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 1.3, 2018-11-04
+ * @version 1.3.1, 2018-11-05
  * @since 2.0
  */
 public enum LevelOfMeasurement {
@@ -55,9 +55,10 @@ public enum LevelOfMeasurement {
     }
       
     /**
-     * Indicates, if this is a quantitative {@link LevelOfMeasurement}, a value that can be compared, sorted or calculated with.
+     * Indicates, if this is a quantitative {@link LevelOfMeasurement}, a value that can be semantically compared, sorted or calculated with.
      * @return {@code true} for a quantitative {@link LevelOfMeasurement} 
-     * @see <a href="https://en.wikipedia.org/wiki/Quantitative">Wikipedia: Quantitative</a> 
+     * @see <a href="https://en.wikipedia.org/wiki/Quantitative">Wikipedia: Quantitative</a>
+     * @see {@link Comparable}
      */
     public boolean isQuantitative() {
         return quantitative;
