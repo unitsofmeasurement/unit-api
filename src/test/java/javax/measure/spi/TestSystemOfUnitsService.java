81f9cf5c0b65a72ded22cf8e5dfd5dc02726842c
Werner Keil <werner.keil@gmx.net>
2020-03-07 21:46:37 +0100

    33: Update TestNG
    Task-Url: https://github.com/unitsofmeasurement/unit-tck/issues/33

--- src/test/java/javax/measure/spi/TestSystemOfUnitsService.java
+++ src/test/java/javax/measure/spi/TestSystemOfUnitsService.java
@@ -1,6 +1,6 @@
 /*
  * Units of Measurement API
- * Copyright (c) 2014-2020, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ * Copyright (c) 2014-2019, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
  *
  * All rights reserved.
  *
