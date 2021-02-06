b5985e3a642925ed1316cae631321d0edd502b9d
Werner Keil <werner.keil@gmx.net>
2021-02-07 00:32:36 +0100

    226: Upgrade JDK to Java 11 or higher
    Task-Url: https://github.com/unitsofmeasurement/unit-api/issues/226

--- src/test/java/javax/measure/test/format/MeasurementParseTest.java
+++ src/test/java/javax/measure/test/format/MeasurementParseTest.java
@@ -1,6 +1,6 @@
 /*
  * Units of Measurement API
- * Copyright (c) 2014-2020, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ * Copyright (c) 2014-2021, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
  *
  * All rights reserved.
  *
--- src/test/java/javax/measure/test/format/package-info.java
+++ src/test/java/javax/measure/test/format/package-info.java
@@ -1,6 +1,6 @@
 /*
  * Units of Measurement API
- * Copyright (c) 2014-2020, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ * Copyright (c) 2014-2021, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
  *
  * All rights reserved.
  *
@@ -27,7 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 /**
  * @author Werner
  *
